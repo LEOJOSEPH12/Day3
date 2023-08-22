ff3427c6c6d78a7c3a8595d1e72726a89bb97e9b
LEOJOSEPH12 <128042183+LEOJOSEPH12@users.noreply.github.com>
2023-08-22 11:20:30 +0530

    Add files via upload

---
+++ api day3/day3/src/main/java/com/demo/day3/Controller/Excercise1.java
@@ -0,0 +1,19 @@
+package com.demo.day3.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Excercise1 {
+@Value("10")
+public int id;
+@Value("Leo")
+public String name;
+@Value("19")
+public int age;
+@Value("20000")
+public long salary;
+@RequestMapping("/day")
+public String day() {
+	return id+"-"+name+"-"+age+"-"+salary;
+}
+}
